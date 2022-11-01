a0145b1692cfd3dc988e35d21339d139602a9792
Ellen Cristina Pelissari Conrado <ellenc.conrado@gmail.com>
2022-11-01 00:01:12 -0300

    finalização

---
+++ exercicios/ex022/index2.js
@@ -0,0 +1,10 @@
+let num = [5,8,4];
+
+console.log(`O vetor tem ${num.length} posições`);
+
+num[3] = 2;
+num.push(7);
+num.sort();
+
+console.log(num);
+console.log(`Agora vetor tem ${num.length} posições`);
